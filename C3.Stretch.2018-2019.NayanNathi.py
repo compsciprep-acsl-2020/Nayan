3cdc328725fe8f0d9e30d8b7ae14be9a21b5d2b7
Nayan Nathi <55326684+NayanN55@users.noreply.github.com>
2020-02-06 18:03:25 -0500

    Update C3.Stretch.2018-2019.NayanNathi.py

--- C3.Stretch.2018-2019.NayanNathi.py
+++ C3.Stretch.2018-2019.NayanNathi.py
@@ -6,11 +6,9 @@
 # Program:Junior Division: Stretch
 
 for Line in (open("Stretchtxt","r")):
-    
     Final = []
-    lastcolumn = []
-    Final_check = []
-    
+    LastColumn = []
+
     Row = int(Line.split(" ")[0])
     Column= int(Line.split(" ")[1])
     Start = int(Line.split(" ")[2])
@@ -18,39 +16,38 @@ for Line in (open("Stretchtxt","r")):
     LastNumber = Start - 1
     
     if NumberOfBlocked > 0:
-        blocked = Line.split(" ")[4:]
+        Blocked = Line.split(" ")[4:]
     else:
-        blocked = [0]
-        
+        Blocked = [0]
+
     for Number in range(0, Row + 1):
-        blocked.append((Number * Column) + 1)
-        
+        Blocked.append((Number * Column) + 1)
+
     for Number in range((Row * Column) + 1, ((Row * Column) + 12)):
-        blocked.append(Number)
-    blocked.remove(Start)
+        Blocked.append(Number)
+    Blocked.remove(Start)
+
+    for Number in Blocked:
+        Blocked[Blocked.index(Number)]=int(Number)
     
-    for Number in blocked:
-        blocked[blocked.index(Number)]=int(Number)
     Number = 0
-    
     while (Number != Row * Column):
         Number = Number + Column
-        lastcolumn.append(Number)
-        
+        LastColumn.append(Number)
+
     if Start == 1:
         pass
     else:
-        lastcolumn.remove(Start - 1)
+        LastColumn.remove(Start - 1)
 
-    while(not(LastNumber in lastcolumn)):
-        
+    while(not(LastNumber in LastColumn)):
         LastNumberset_A = LastNumber
-        
-        if not(LastNumber + 1 in blocked):
+
+        if not(LastNumber + 1 in Blocked):
             LastNumber = LastNumber + 1
-            if not (LastNumber + 1 in blocked):
+            if not (LastNumber + 1 in Blocked):
                 LastNumber = LastNumber + 1
-                if not (LastNumber + 1 in blocked):
+                if not (LastNumber + 1 in Blocked):
                     LastNumber = LastNumber + 1
                     Final.append("A")
                 else:
@@ -59,14 +56,14 @@ for Line in (open("Stretchtxt","r")):
                 LastNumber = LastNumberset_A
         else:
             LastNumber = LastNumberset_A
-            
+
         LastNumberset_B = LastNumber
-        
-        if not(LastNumber + 1 in blocked):
+
+        if not(LastNumber + 1 in Blocked):
             LastNumber = LastNumber+1
-            if not (LastNumber + Column in blocked):
+            if not (LastNumber + Column in Blocked):
                 LastNumber = LastNumber + Column
-                if not (LastNumber + 1 in blocked):
+                if not (LastNumber + 1 in Blocked):
                     LastNumber = LastNumber + 1
                     Final.append("B")
                 else:
@@ -75,16 +72,16 @@ for Line in (open("Stretchtxt","r")):
                 LastNumber = LastNumberset_B
         else:
             LastNumber=LastNumberset_B
-            
+
         LastNumberset_C = LastNumber
-        
-        if not(LastNumber + 1 in blocked):
+
+        if not(LastNumber + 1 in Blocked):
             LastNumber = LastNumber+1
-            if not (LastNumber + 1 in blocked):
+            if not (LastNumber + 1 in Blocked):
                 LastNumber = LastNumber + 1
-                if not (LastNumber + Column in blocked):
+                if not (LastNumber + Column in Blocked):
                     LastNumber = LastNumber + Column
-                    if not (LastNumber + Column in blocked):
+                    if not (LastNumber + Column in Blocked):
                         LastNumber = LastNumber + Column
                         Final.append("C")
                     else:
